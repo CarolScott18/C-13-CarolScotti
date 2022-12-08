7a766ef8982396fb4005062d80b4d2e3c6c7d1c6
CarolScott18 <119981417+CarolScott18@users.noreply.github.com>
2022-12-07 22:31:49 -0300

    C-13 carol

---
+++ C13-SA1-main/sketch.js
@@ -0,0 +1,124 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage,cloudImage,cloud, obstacle;
+var obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+
+
+
+var score;
+
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
+  trex_collided = loadImage("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+ obtacle1 = loadImage("obstacle1.png");
+ obtacle2 = loadImage("obstacle2.png");
+ obtacle3 = loadImage("obstacle3.png");
+ obtacle4 = loadImage("obstacle4.png");
+ obtacle5 = loadImage("obstacle5.png");
+ obtacle6 = loadImage("obstacle6.png");
+
+  
+}
+
+function setup() {
+
+  createCanvas(600,200)
+  
+  //crie um sprite de trex
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  
+  //crie um sprite ground (solo)
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  //crie um solo invisível
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  //gerar números aleatórios
+ // var rand =  Math.round(random(1,100))
+ // console.log(rand)
+
+}
+
+function draw() {
+  //definir cor de fundo
+  background("White");
+  
+  
+  
+  
+  
+  // pular quando tecla espaço for pressionada
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  //impedir que o trex caia
+  trex.collide(invisibleGround);
+  
+  //gerar as nuvens
+  spawnClouds();
+  spawnObstacle();
+  drawSprites();
+}
+
+//função para gerar as nuvens
+function spawnClouds(){
+ if(frameCount % 60 === 0){
+
+  // escreva seu código aqui
+ cloud=createSprite(600,100,40,10);
+ cloud.addImage(cloudImage);
+ cloud.y = Math.round(random(10,60));
+ cloud.scale = 1.2;
+ cloud.velocityX=-3;
+ 
+cloud.lifetime = 200;
+
+cloud.depth = trex.depth;
+trex.depth = trex.depth + 1
+
+}
+}
+function spawnObstacle(){
+if(frameCount % 60 === 0){ 
+obstacle = createSprite(600,165,10,40);
+obstacle.velocityX = -6;
+
+rand = Math.round(random(1,6));
+
+switch(rand){
+  case 1: obstacle.addImage(obstacle1);
+          break;
+  case 2: obstacle.addImage(obstacle2);
+          break;
+  case 3: obstacle.addImage(obstacle3);
+          break;
+  case 4: obstacle.addImage(obstacle4);
+          break;
+  case 5: obstacle.addImage(obstacle5);
+          break;
+  case 6: obstacle.addImage(obstacle6);
+          break;
+  default:break;
+
+}
+}
+}
+
+
